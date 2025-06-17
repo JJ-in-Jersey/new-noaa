c03a51dfd61513dfc153625f204c000db01e3a35
JJ-in-Jersey <jason.bronfeld@pm.me>
2025-06-17 13:02:15 -0400

    added remove adjusted to spline code

--- main.py
+++ main.py
@@ -58,6 +58,7 @@ class SplineCSV:
             cs_frame['Time'] = to_datetime(cs_frame.stamp, unit='s').dt.tz_localize('UTC')
             cs_frame['Velocity_Major'] = cs_frame.Velocity_Major.round(2)
             cs_frame.write(waypoint.velocity_csv_path)
+            remove(waypoint.adjusted_csv_path)
             self.success = True
             self.failure_message = None
         except Exception as e:
