efa467da574f40e73d540222ead8077d40ae1a6a
jj-in-jersey <faircurrents@gmail.com>
2025-11-26 16:09:50 -0500

    changed the approach. Writing takes place in the execute_callback function. Changed names from xxxCSV to xxxFrame main.py

--- main.py
+++ main.py
@@ -1,79 +1,12 @@
 from argparse import ArgumentParser as argParser
+import shutil
 
 from tt_dictionary.dictionary import Dictionary
 import tt_globals.globals as Globals
 from tt_job_manager.job_manager import JobManager
-from tt_jobs.jobs import RequestVelocityJob, SplineJob
+from tt_jobs.jobs import RequestVelocityJob, SplineJob, RequestVelocityFrame, SplineFrame
 from tt_noaa_data.noaa_data import StationDict
 from tt_gpx.gpx import Waypoint
-
-# class RequestVelocityCSV:
-#     def __init__(self, year: int, waypoint: Waypoint):
-#         self.id = waypoint.id
-#
-#         if waypoint.type == "H":
-#             path = waypoint.velocity_csv_path
-#         elif waypoint.type == 'S':
-#             path = waypoint.adjusted_csv_path
-#         else:
-#             raise TypeError
-#
-#         if not path.exists():
-#             try:
-#                 sixteen_months = SixteenMonths(year, waypoint)
-#                 sixteen_months.write(path)
-#                 self.success = True
-#                 self.failure_message = None
-#             except Exception as e:
-#                 self.success = False
-#                 self.failure_message = f'<!> {waypoint.id} {type(e).__name__}'
-#                 print(self.failure_message)
-#
-#
-# # noinspection PyShadowingNames
-# class RequestVelocityJob(Job):  # super -> job name, result key, function/object, arguments
-#     def execute(self): return super().execute()
-#     def execute_callback(self, result): return super().execute_callback(result)
-#     def error_callback(self, result): return super().error_callback(result)
-#
-#     def __init__(self, year, waypoint: Waypoint):
-#         result_key = id(waypoint)
-#         arguments = tuple([year, waypoint])
-#         super().__init__(waypoint.id + ' ' + waypoint.name, result_key, RequestVelocityCSV, arguments, {})
-
-# class SplineCSV:
-#     def __init__(self, year: int, waypoint: Waypoint):
-#         self.id = waypoint.id
-#
-#         try:
-#             stamp_step = 60  # timestamps in seconds so steps of one minute is 60
-#             start_stamp = int(datetime(year=year - 1, month=11, day=1).timestamp())
-#             end_stamp = int(datetime(year=year + 1, month=3, day=1).timestamp())
-#             stamps = [start_stamp + i * stamp_step for i in range(int((end_stamp - start_stamp)/stamp_step))]
-#
-#             input_frame = DataFrame(csv_source=waypoint.adjusted_csv_path)
-#             cs_frame = CubicSplineFrame(input_frame.stamp, input_frame.Velocity_Major, stamps)
-#             cs_frame['Time'] = to_datetime(cs_frame.stamp, unit='s').dt.tz_localize('UTC')
-#             cs_frame['Velocity_Major'] = cs_frame.Velocity_Major.round(2)
-#             cs_frame.write(waypoint.velocity_csv_path)
-#             remove(waypoint.adjusted_csv_path)
-#             self.success = True
-#             self.failure_message = None
-#         except Exception as e:
-#             self.success = False
-#             self.failure_message = f'<!> {waypoint.id} {type(e).__name__}'
-#             print(self.failure_message)
-#
-# class SplineJob(Job):  # super -> job name, result key, function/object, arguments
-#     def execute(self): return super().execute()
-#     def execute_callback(self, result): return super().execute_callback(result)
-#     def error_callback(self, result): return super().error_callback(result)
-#
-#     def __init__(self, year: int, waypoint: Waypoint):
-#         result_key = id(waypoint.id)
-#         arguments = tuple([year, waypoint])
-#         super().__init__(waypoint.id + ' ' + waypoint.name, result_key, SplineCSV, arguments, {})
-
 
 if __name__ == '__main__':
     ap = argParser()
@@ -93,6 +26,7 @@ if __name__ == '__main__':
         station_dict = StationDict(json_source=Globals.STATIONS_FILE)
     else:
         station_dict = StationDict()
+
     waypoint_dict = Dictionary({key: Waypoint(station_dict[key]) for key in station_dict.keys() if not ('#' in key or station_dict[key]['type'] == 'W')})
     for wp in [wp for wp in waypoint_dict.values() if not Globals.GPX_FOLDER.joinpath(wp.id + '.gpx').exists()]:
         wp.write_gpx()
@@ -102,9 +36,7 @@ if __name__ == '__main__':
     # job_manager = None
 
     print(f'Requesting velocity data for each waypoint')
-    waypoints = [w for w in waypoint_dict.values()
-                 if not (w.velocity_csv_path.exists() or w.adjusted_csv_path.exists())
-                 and (w.type == 'H' or w.type == 'S')]
+    waypoints = [w for w in waypoint_dict.values() if not w.raw_csv_path.exists() and (w.type == 'H' or w.type == 'S')]
 
     while len(waypoints):
         print(f'\nLength of list: {len(waypoints)}')
@@ -117,6 +49,7 @@ if __name__ == '__main__':
 
         print(f'\nProcessing velocity data results')
         results_dict = {wp_id: job_manager.get_result(wp_id) for wp_id in keys}
+        success_dict = {k: v for k, v in results_dict.items() if isinstance(v, RequestVelocityFrame)}
         error_dict = {k: v for k, v in results_dict.items() if isinstance(v, Exception)}
         for wp_id, result in error_dict.items():
             yn = input(f'Exclude {result.__class__.__name__} {wp_id} from StationDict processing? (y/n): ').lower()
@@ -124,9 +57,11 @@ if __name__ == '__main__':
                 station_dict.comment_waypoint(wp_id)
                 waypoint_dict.pop(wp_id)
 
-        waypoints = [w for w in waypoint_dict.values()
-                     if not (w.velocity_csv_path.exists() or w.adjusted_csv_path.exists())
-                     and (w.type == 'H' or w.type == 'S')]
+        waypoints = [w for w in waypoint_dict.values() if not w.raw_csv_path.exists() and (w.type == 'H' or w.type == 'S')]
+
+    yn = input(f'Did everything download successfully? (y/n): ').lower()
+    if yn == 'n' or yn == 'no':
+        exit(0)
 
     print(f'\nSpline fitting subordinate waypoints')
     subordinate_waypoints = [wp for wp in waypoint_dict.values() if wp.type == 'S' and not wp.velocity_csv_path.exists()]
@@ -137,10 +72,19 @@ if __name__ == '__main__':
     job_manager.wait()
 
     print(f'\nProcessing spline fitting results')
-    for result in [r for r in [job_manager.get_result(key) for key in keys] if not r.success]:
-        yn = input(f'Exclude {result.failure_message} from StationDict processing? (y/n): ').lower()
+    results_dict = {wp_id: job_manager.get_result(wp_id) for wp_id in keys}
+    success_dict = {k: v for k, v in results_dict.items() if isinstance(v, SplineFrame)}
+    error_dict = {k: v for k, v in results_dict.items() if isinstance(v, Exception)}
+    for wp_id, result in error_dict.items():
+        yn = input(f'Exclude {result.__class__.__name__} {wp_id} from StationDict processing? (y/n): ').lower()
         if yn == 'y' or yn == 'yes':
-            station_dict.comment_waypoint(result.id)
-            waypoint_dict.pop(result.id)
+            station_dict.comment_waypoint(wp_id)
+            waypoint_dict.pop(wp_id)
+
+    print(f'\nCopying harmonic results that do not need fitting')
+    harmonic_waypoints = [wp for wp in waypoint_dict.values() if wp.type == 'H' and not wp.velocity_csv_path.exists()]
+    for wp in harmonic_waypoints:
+        shutil.copy(wp.raw_csv_path, wp.velocity_csv_path)
 
     job_manager.stop_queue()
+
